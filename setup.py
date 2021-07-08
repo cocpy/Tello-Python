eec12146dd05f289493e088320ebad4519bba967
c0c <15754324003@163.com>
2021-07-08 15:43:15 +0800

    自定义视频流窗口大小

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='tello-python',
-    version='1.1.3',
+    version='1.1.4',
     author='C灵C',
     author_email='c0c@cocpy.com',
     description='Control DJI Tello drone with Python3',
